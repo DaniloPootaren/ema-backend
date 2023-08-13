d9592af7bc18760c60f4250592f3c091cf713705
Jean Danilo Maxwell Patrick Pootaren <danilo.pootaren@mns.mu>
2023-08-13 22:20:24 +0400

    Added content types

---
+++ src/api/reported-article/routes/reported-article.ts
@@ -0,0 +1,7 @@
+/**
+ * reported-article router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::reported-article.reported-article');
---
+++ src/api/reported-article/services/reported-article.ts
@@ -0,0 +1,7 @@
+/**
+ * reported-article service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::reported-article.reported-article');
